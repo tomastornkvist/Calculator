2a92858b6e050744bcdb114d1dde53695db8a20e
tomastornkvist <tomas.tornkvist@gmail.com>
2026-09-02 17:10:33 +0200

    First working version

--- Program.cs
+++ Program.cs
@@ -1,20 +1,7 @@
-﻿static int ReadOperator()
-{
-    int op;
-
-
-    Console.Write("Räknesätt (+ - * / % =): ");
-
-    while (!int.TryParse(Console.ReadLine(), out op))
-        Console.Write("Du måste ange ett giltigt räknesätt (+ - * / % =): ");
-
-    return op;
-}
-
+﻿// Reads a number from the commandline and makes sure it's a valid double
 static double ReadNumber()
 {
     double number;
-
 
     Console.Write("Tal: ");
 
@@ -23,4 +10,79 @@ static double ReadNumber()
 
     return number;
 }
+
+// Reads an operator from the commandline and makes sure it's one of +, -, *, /, % or =.
+static string ReadOperator()
+{
+    string? op;
+    bool correct = false;
+
+
+    Console.Write("Räknesätt (+ - * / % =): ");
+
+    do
+    {
+        switch (op = Console.ReadLine())
+        {
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+            case "%":
+            case "=":
+                correct = true;
+                break;
+
+            default:
+                Console.Write("Du måste ange ett giltigt räknesätt (+ - * / % =): ");
+                break;
+        }
+    } while (!correct);
+
+    return op!;
+}
+
+string op;
+double currentResult = ReadNumber();
+
+do
+{
+    op = ReadOperator();
+
+    if (op != "=")
+    {
+        double number = ReadNumber();
+
+        switch (op)
+        {
+            case "+":
+                currentResult += number;
+                Console.WriteLine($"= {currentResult}");
+                break;
+
+            case "-":
+                currentResult -= number;
+                Console.WriteLine($"= {currentResult}");
+                break;
+
+            case "*":
+                currentResult *= number;
+                Console.WriteLine($"= {currentResult}");
+                break;
+
+            case "/":
+                currentResult /= number;
+                Console.WriteLine($"= {currentResult}");
+                break;
+
+            case "%":
+                currentResult %= number;
+                Console.WriteLine($"= {currentResult}");
+                break;
+        }
+    }
+
+} while (!op.Equals("="));
+
+Console.WriteLine($"Slutresultat: {currentResult}");
 
