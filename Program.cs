fa1f9eb2d2900a23bca9baf2ba55f1df426c545f
tomastornkvist <tomas.tornkvist@gmail.com>
2026-09-02 15:03:42 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿static int ReadOperator()
+{
+    int op;
+
+
+    Console.Write("Räknesätt (+ - * / % =): ");
+
+    while (!int.TryParse(Console.ReadLine(), out op))
+        Console.Write("Du måste ange ett giltigt räknesätt (+ - * / % =): ");
+
+    return op;
+}
+
+static double ReadNumber()
+{
+    double number;
+
+
+    Console.Write("Tal: ");
+
+    while (!double.TryParse(Console.ReadLine(), out number))
+        Console.Write("Du måste ange ett giltigt tal: ");
+
+    return number;
+}
+
